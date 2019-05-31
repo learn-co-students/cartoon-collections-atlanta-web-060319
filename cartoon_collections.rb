ddd73edc2b223413b5705ef616ca9a275ae79ec8
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-31 05:09:35 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+  ["Doc", "Dopey", "Bashful", "Grumpy"].each_with_index do |value, index|
+    puts "#{index}: #{value}"
 end
 
 def summon_captain_planet# code an argument here
